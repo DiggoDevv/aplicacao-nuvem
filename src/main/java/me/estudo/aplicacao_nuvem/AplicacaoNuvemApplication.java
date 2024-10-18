96e263625b2053c88eb5e517eb77c3d3c6c39797
Rodrigo <rodrigobaalves.2004@gmail.com>
2024-10-18 11:46:26 -0300

    Initial commit

---
+++ src/main/java/me/estudo/aplicacao_nuvem/AplicacaoNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.estudo.aplicacao_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/estudo/aplicacao_nuvem/AplicacaoNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.estudo.aplicacao_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
